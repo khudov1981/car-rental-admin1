6548715e30f56926353b1415e6a179a533f5b3e1
smsforward2 <smsforward2@sourcecraft.dev>
2025-11-21 17:49:40 +0000

    Рефакторинг кода в src/utils/formatters.js для улучшения переиспользуемости

---
+++ src/utils/formatters.js
@@ -0,0 +1,34 @@
+// Общая функция для очистки и форматирования значения
+const cleanAndFormat = (value, pattern) => {
+  if (!value) return ''
+  
+  // Очистка значения от лишних символов
+  let cleanValue = value.replace(/[^A-Z0-9А-Я]/g, '')
+  
+  // Форматирование в соответствии с шаблоном
+  let formattedValue = ''
+  let cleanIndex = 0
+  
+  for (let i = 0; i < pattern.length && cleanIndex < cleanValue.length; i++) {
+    if (pattern[i] === 'X' || pattern[i] === '0') {
+      formattedValue += cleanValue[cleanIndex]
+      cleanIndex++
+    } else {
+      formattedValue += pattern[i]
+    }
+  }
+  
+  return formattedValue
+}
+
+// Форматирование отображаемого значения госномера
+export const formatPlateDisplay = (value) => {
+  // Формат: X000XX000 (1 буква, 3 цифры, 2 буквы, 2-3 цифры региона)
+  return cleanAndFormat(value, 'X000XX000')
+}
+
+// Форматирование отображаемого значения ОСАГО
+export const formatInsuranceDisplay = (value) => {
+  // Формат: XXX0000000000 (3 буквы, 10 цифр)
+  return cleanAndFormat(value, 'XXX0000000000')
+}
